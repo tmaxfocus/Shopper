4a30b0492ca1db0f737f7f0a803d9b7fc63b2ca0
Tobi Adeogun <Tobi.Adeogun@stanbicibtc.com>
2024-04-05 10:01:45 +0100

    seeding data

---
+++ Shoppers.Delight/Catalog.Infrastructure/Data/ProductContextSeed.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class ProductContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool checkBrand = productCollection.Find(a => true).Any();
+            string path = Path.Combine("Data", "SeedData", "products.json");
+            if (!checkBrand)
+            {
+                var brandsData = File.ReadAllText(path);
+                var brands = JsonSerializer.Deserialize<List<Product>>(brandsData);
+                if (brands != null)
+                {
+                    foreach (var productBrand in brands)
+                    {
+                        productCollection.InsertOneAsync(productBrand);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Shoppers.Delight/Catalog.Infrastructure/Data/TypeContextSeed.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class TypeContextSeed
+    {
+        public static void SeedData(IMongoCollection<ProductType> typeCollection)
+        {
+            bool checkBrand = typeCollection.Find(a => true).Any();
+            string path = Path.Combine("Data", "SeedData", "type.json");
+            if (!checkBrand)
+            {
+                var brandsData = File.ReadAllText(path);
+                var brands = JsonSerializer.Deserialize<List<ProductType>>(brandsData);
+                if (brands != null)
+                {
+                    foreach (var productBrand in brands)
+                    {
+                        typeCollection.InsertOneAsync(productBrand);
+                    }
+                }
+            }
+        }
+    }
+}
