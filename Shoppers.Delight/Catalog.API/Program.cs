40cb9d19db7f5626c2f8444361756fbacdc9e10a
Tobi Adeogun <Tobi.Adeogun@stanbicibtc.com>
2024-04-11 09:12:11 +0100

    ProductBrand CQRS

--- Shoppers.Delight/Catalog.API/Program.cs
+++ Shoppers.Delight/Catalog.API/Program.cs
@@ -1,10 +1,15 @@
+using Catalog.Core.Repositories;
 using Catalog.Infrastructure.Data;
+using Catalog.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddTransient<ICatalogContext, CatalogContext>();
+builder.Services.AddTransient<IProductRepository, ProductRespository>();
+builder.Services.AddTransient<IBrandRepsository, ProductRespository>();
+builder.Services.AddTransient<ITypesRepository, ProductRespository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ Shoppers.Delight/Catalog.Application/Handlers/GetAllBrandsHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetAllBrandsHandler : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+    {
+        private readonly IBrandRepsository _brandRepsository;
+        private readonly IMapper _mapper;
+
+        public GetAllBrandsHandler(IBrandRepsository brandRepsository, IMapper mapper)
+        {
+            _brandRepsository = brandRepsository;
+            _mapper = mapper;
+        }
+        public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+        {
+            var brandList = await _brandRepsository.GetAllBrands();
+            var brandReponseList = _mapper.Map<IList<BrandResponse>>(brandList);
+            return brandReponseList;
+        }
+    }
+}
